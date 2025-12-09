6892167d831b409a9766d358682289eb344dd4fd
saloni singh <saloni.singh301295@gmail.com>
2025-12-09 18:03:54 +0530

    fix auth routes

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, callback } from "../controller/auth.controller.js";
+import { login, callback } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
